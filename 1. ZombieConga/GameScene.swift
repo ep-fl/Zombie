a715f14821b5f9d9b1f56cca89e6d2b6cac0894e
ep_fl <socialinfo.my@gmail.com>
2015-02-03 12:38:18 +0300

    corretc scene

--- 1. ZombieConga/GameScene.swift
+++ 1. ZombieConga/GameScene.swift
@@ -11,5 +11,13 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         self.backgroundColor = SKColor.whiteColor()
+        
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        
+//        background.zRotation = CGFloat(M_PI) / 8
+        
+        self.addChild(background)
     }
 }
